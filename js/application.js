e48b7dad3a2c9adbb116dc5d4e02c6b321ff4cfd
Nathan Sass <nsass711@gmail.com>
2014-05-19 14:47:19 -0700

    using fixed background

--- js/application.js
+++ js/application.js
@@ -1,18 +1,19 @@
-// $(document).ready(function(){
-// 		var $window = $(window)    
+$(document).ready(function(){
+	
+	$(document).scroll(function(e){
+		// console.log(document.body.scrollTop)
+		if (document.body.scrollTop >= 470){
+		// debugger
+			// $('#background-image').removeClass('beautiful-js').addClass('beautiful-truchas')
+			// console.log("above 300");
+		};
+	});
 
-//     $('section[data-type="background"]').each(function(){
-//         var $bgobj = $(this); // assigning the object
-     
-//         $(window).scroll(function() {
-//         	// var yPos = -( ($window.scrollTop() - $bgobj.offset().top) / $bgobj.data('speed'));
-//             var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
-             
-//             // Put together our final background position
-//             var coords = '50% '+ yPos + 'px';
- 
-//             // Move the background
-//             $bgobj.css({ backgroundPosition: coords });
-//         }); 
-//     });    
+
+});
+
+
+
+// $( "#target" ).scroll(function() {
+//   $( "#log" ).append( "<div>Handler for .scroll() called.</div>" );
 // });
