b3f2379fa16128dfb0cfc969c9e75edef4bd3d68
Nathan Sass <nsass711@gmail.com>
2014-05-20 20:44:05 -0700

    mobile mvp

--- js/application.js
+++ js/application.js
@@ -1,6 +1,7 @@
 function detectmob() {
  if((window.innerWidth <= 800) && (window.innerHeight <= 600)) {
-   return true;
+ 		console.log("here")
+   $(".polemic").prepend('<div class="mobile-tagline"><p>A Fearless Creative</p></div>')
    } else {
    	var s = skrollr.init();
    }
